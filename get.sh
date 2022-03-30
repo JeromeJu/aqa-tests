ce06430fab2bf22db709931131d134bd44f7f435
JeromeJu <jeromeqju@gmail.com>
2022-03-30 22:58:09 +0800

    test

--- get.sh
+++ get.sh
@@ -602,6 +602,7 @@ testJavaVersion()
 		if [ "$os" = "windows" ]; then
 			TEST_JDK_HOME=$(cygpath -w "${TEST_JDK_HOME}")
 		fi
+		echo ${TEST_JDK_HOME}
 		echo -e "TEST_JDK_HOME=${TEST_JDK_HOME}" > ${TESTDIR}/job.properties
 		if [ -e ${_release} ]; then
 			echo "=RELEASE INFO BEGIN="
